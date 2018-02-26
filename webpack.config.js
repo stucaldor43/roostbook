c45075191b2792ab2edd9646ed7a97f9e58e2504
Anthony Cook <ddragonmaster49@yahoo.com>
2018-02-25 23:55:35 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [ 
+                    { 
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["es2015", "react"]
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: [ 
+                    { 
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["es2015", "react"]
+                        }
+                    }
+                ],
+            },
+            // {
+            //     test: /\.css$/,
+            //     exclude: /node_modules/,
+            //     use: [
+            //         {
+            //             loader: 'style-loader'
+            //         },
+            //         {
+            //             loader: 'css-loader',
+            //             options: {
+            //                 modules: true
+            //             }
+            //         }
+            //     ]
+            // },
+            // {
+            //     test: /\.useable\.css$/,
+            //     exclude: /node_modules/,
+            //     use: [
+            //         {
+            //             loader: 'style-loader/useable'
+            //         },
+            //         {
+            //             loader: 'css-loader',
+            //             options: {
+            //                 modules: true
+            //             }
+            //         }
+            //     ]
+            // }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "RoostBook",
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 8080,
+            files: ["dist/**/*.*", "stylesheets/**/*.*"],
+            server: {
+                baseDir: "./",
+                index: "index.html"
+            }
+        })
+    ]
+}
