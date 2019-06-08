a9f80b81c109f7e1d58c146c652d80df3dd13d00
Anthony Cook <ddragonmaster49@yahoo.com>
2019-06-08 16:21:48 -0400

    Add goToPage prop

--- src/presentational/Pagination.jsx
+++ src/presentational/Pagination.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './../../stylesheets/components/Pagination/styles.css';
 
-const Pagination = ({url, page, maxPages}) => {
-    const previousPage = (page > 1) ? <a className="pagination-previousPageLink pagination-link" 
-                                         href="">&#9668;</a> : null;
-    const nextPage = ((page + 1) <= maxPages) ? <a className="pagination-nextPageLink pagination-link" 
-                                                   href="">&#9658;</a> : null;
+const Pagination = ({url, page, maxPages, goToPage}) => {
+    const previousPage = (page > 1) ? <span className="pagination-previousPageLink pagination-link" 
+                                         onClick={() => goToPage(page - 1)}>&#9668;</span> : null;
+    const nextPage = ((page + 1) <= maxPages) ? <span className="pagination-nextPageLink pagination-link" 
+                                                   onClick={() => goToPage(page + 1)}>&#9658;</span> : null;
     const pages = [];
     pages.push(1, '..', page - 1, page, page + 1, '..', maxPages);
     const links = pages.reduce((acc, curr, index, arr) => {
@@ -17,8 +17,9 @@ const Pagination = ({url, page, maxPages}) => {
       }
     }, [])
     .filter((n) => typeof(n) === "string" || n >= 1)
-    .map((text) => (typeof text === "number") ? <a className="pagination-specificPageLink pagination-link" 
-                                                   href="">{ text }</a> : <span className="pagination-ellipsis">{ text }</span>);
+    .filter((el, i, arr) => arr.length === 1 ? false : true)
+    .map((text) => (typeof text === "number") ? <span className={`pagination-specificPageLink pagination-link ${text === page ? 'pagination-currentPageLink' : ''} ?`} 
+                                                   onClick={() => goToPage(text)}>{ text }</span> : <span className="pagination-ellipsis">{ text }</span>);
     
     return (
         <div className="pagination">
